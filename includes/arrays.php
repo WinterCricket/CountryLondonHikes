8caab459cbaf8603dfd852d21e4a3c7c18e4cc32
WinterCricket <hodhodhod2003@gmail.com>
2020-11-30 22:05:29 -0600

    begin to convert static site to dynamic php

---
+++ includes/arrays.php
@@ -0,0 +1,62 @@
+<?php 
+
+//nav menu items
+
+$navItems = array(
+
+	array(
+		"slug" 		=> "index.php",
+		"title" 	=> "Home"
+	),
+	array(
+		"slug" 		=> "team.php",
+		"title"		=> "Team"
+	),
+	array(
+		"slug" 		=> "menu.php",
+		"title" 	=> "Menu"
+	),
+	array(
+		"slug" 		=> "contact.php",
+		"title" 	=> "Contact"
+	),
+);
+
+
+
+
+
+//Menu Items using custom keys
+
+$trails = array(
+	"slug-sandwich" => array(
+
+		"title"		=> "Slug Sandwich ",
+		"price"		=> 11,
+		"blurb"		=> "People do order this sandwich, disbelieving that we would serve them actual slugs. We collect the slugs from Maurices garden and Lulebells pear orchard and put them in the kitchen cooler. Sliced apples or pears on oven fresh buttermilk sourdough (thick cuts!) slathered in fresh Vermont butter from the creamer. Then we add the onion sauteed lightly salted slugs. No one has finished the sandwich entirely. Btw: slugs are poisonous to humans, even when cooked.",
+		"drink" 	=> "White Vodka"
+	),
+	"lime-dill-salmon" => array(
+
+		"title"		=> "Lime &amp; Dill Salmon ",
+		"price"		=> 22,
+		"blurb"		=> "We had this two days a week during our months in the Caribean. The salmon is grilled in blonde olive oil with some Bahamian grasses to a fine, delicate bark with ground pepper. The last moments of cooking and while the fish rests in the plate, we let whole lime pulp and dill grass marinate on the meat.",
+		"drink" 	=> "Club Soda"
+	),
+	"chocolate-soup" => array(
+
+		"title"		=> "Hot Chocolate Soup<sup>&reg;</sup> ",
+		"price"		=> 48,
+		"blurb"		=> "Oh, yes. It's a real soup. It tastes as good as it's name suggests. We serve cream-of-chocolate soup as well, but the claim of our fame rests with this straight mole and hot pepper concoction, a love potion for the holidays.",
+		"drink" 	=> "Pomegranite Water"
+	),
+	"ivory-salad" => array(
+
+		"title"		=> "Ivory Dikon Salad ",
+		"price"		=> 21,
+		"blurb"		=> "It's spicey. It's mediterranean. It goes with green wine. Presto!",
+		"drink" 	=> "Retsina"
+	),
+);
+
+ ?>
