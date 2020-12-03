03483fd114d469ebf58804d1fcab561c305cde8d
WinterCricket <hodhodhod2003@gmail.com>
2020-12-02 23:43:43 -0600

    renamed variables and added html to hike

--- hike.php
+++ hike.php
@@ -12,9 +12,9 @@ function strip_bad_chars($input){
 }
 
 if (isset($_GET['item'])){
-	$menuItem = strip_bad_chars($_GET['item']);
+	$trailOption = strip_bad_chars($_GET['item']);
 
-	$dish = $menuItems[$menuItem];
+	$trail = $trailOptions[$trailOption];
 }
 
 //calculate tip
@@ -27,3 +27,15 @@ function suggestedTip($price, $tip){
 
 
  ?>
+
+ <hr>
+ <div id="hike">
+ 	<h1><?php echo $trail['title']; ?><span class="price"><sup>$</sup><?php echo $trail['price']; ?></span></h1>
+ 	<p><?php echo $trail['blurb']; ?></p>
+ 	<br>
+ 	<!-- php in here for drink -->
+ 	<p><em>Suggested Tip: <sup>$</sup><?php suggestedTip($trail['price'], 0.20); ?></em></p>
+ </div><!--dish-->
+ <hr>
+ <a href="trail_options.php" class="button previous">&laquo; Back to Menu</a>
+ <?php include('includes/footer.php'); ?>
--- trail_options.php
+++ trail_options.php
@@ -9,7 +9,7 @@ include('includes/header.php');
 
 <div id="trail_options">
 	<h1>Choose a Trail</h1>
-		<p>Each Country London trail as a storied history &mdash; John Rogers will guide you into a lost world and how it comes right up to the present.</p>
+		<p>Each Country London trail as a storied history &mdash; John Rogers will guide you into a lost world, and then show you how it comes right up to the present.</p>
 		<p><em>Please click a menu to learn more!</em></p>
 	<hr>
 
@@ -23,7 +23,7 @@ include('includes/header.php');
 
 		 ?>
 
-		 <li><a href="hike.php?item=<?php echo $trail; ?>"><?php echo $item['title']; ?></a><sup>$</sup><?php echo $item['price']; ?><?php echo $item['blurb']; ?></li>
+		 <li><a href="hike.php?item=<?php echo $trail; ?>"><?php echo $item['title']; ?></a><sup>$</sup><?php echo $item['price']; ?></li>
 
 		<?php } ?>
 
